952a5f00a42b2dc2cbc13f1fec9f41b1e4a22ffd
George Walker <george.walker.wme@gmail.com>
2022-10-27 12:14:39 +0100

    Styling and layout complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+// import {useState} from 'react'
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Memory Game</h1>
+    <div className="container">
+      <header className="d-flex justify-content-between align-items-center my-4">
+        <h1>Memory Game</h1>
+        <ul className="list-group">
+          <li className="list-group-item">Score:</li>
+          <li className="list-group-item">Best Score:</li>
+        </ul>
+      </header>
+      <main className="d-flex justify-content-center">
+        <div className="card card-color" style={{width: '18rem'}}>
+          <img className="card-img-top" src="..." alt="Game of thrones"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
